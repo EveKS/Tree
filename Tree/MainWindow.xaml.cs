6ef0124cc395894b59525ad2c7705826ba4c9670
EveKS <vikkes@yandex.ru>
2017-12-26 09:33:25 +0700

    Добавьте файлы проекта.

---
+++ Tree/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tree
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Point old;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.MiddleButton == MouseButtonState.Pressed)
+            {
+                this.Close();
+
+                e.Handled = true;
+            }
+
+            old = e.GetPosition(null);
+        }
+
+        private void Image_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                Point cur = e.GetPosition(null);
+
+                this.Left += cur.X - old.X;
+                this.Top += cur.Y - old.Y;
+            }
+        }
+
+        #region Alt + Tab
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr window, int index, int value);
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowLong(IntPtr window, int index);
+
+        private const int GWL_EXSTYLE = -20;
+        private const int WS_EX_TOOLWINDOW = 0x00000080;
+
+        public static void HideFromAltTab(IntPtr Handle)
+        {
+            SetWindowLong(Handle,
+                          GWL_EXSTYLE,
+                          GetWindowLong(Handle, GWL_EXSTYLE) | WS_EX_TOOLWINDOW);
+        }
+
+        private IntPtr Handle
+        {
+            get
+            {
+                return new WindowInteropHelper(this).Handle;
+            }
+        }
+        #endregion
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            HideFromAltTab(Handle);
+        }
+
+        #region back window
+        [DllImport("user32.dll")]
+        public static extern bool SetWindowPos(int hWnd, int hWndInsertAfter, int X, int Y,
+            int cx, int cy, uint uFlags);
+
+        public const int HWND_BOTTOM = 0x1;
+        public const uint SWP_NOSIZE = 0x1;
+        public const uint SWP_NOMOVE = 0x2;
+        public const uint SWP_SHOWWINDOW = 0x40;
+
+        private void ShoveToBackground()
+        {
+            SetWindowPos((int)this.Handle, HWND_BOTTOM, 0, 0, 0, 0, SWP_NOMOVE |
+                SWP_NOSIZE | SWP_SHOWWINDOW);
+        }
+        #endregion
+
+        private void Window_Activated(object sender, EventArgs e)
+        {
+            ShoveToBackground();
+        }
+    }
+}
